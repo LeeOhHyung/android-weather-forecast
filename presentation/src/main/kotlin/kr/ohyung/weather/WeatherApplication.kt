1c2111f1fea249d1c620502c6ca9a99f22ee4fa2
leeohhyoung <lee2928030@naver.com>
2020-07-08 00:22:43 +0900

    Application Class로 사용할 WeatherApplication class 추가

---
+++ presentation/src/main/kotlin/kr/ohyung/weather/WeatherApplication.kt
@@ -0,0 +1,22 @@
+package kr.ohyung.weather
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+/**
+ * Created by Lee Oh Hyoung on 2020/07/08.
+ */
+class WeatherApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@WeatherApplication)
+
+        }
+    }
+}
