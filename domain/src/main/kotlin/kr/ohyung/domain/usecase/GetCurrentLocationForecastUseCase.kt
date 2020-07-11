0691024221dd825b39bbc21a4335a1596e561d87
LeeOhHyung <lee2928030@naver.com>
2020-07-11 20:02:41 +0900

    날씨 예보를 가져오는 GetCurrentLocationForecastUseCase 정의

---
+++ domain/src/main/kotlin/kr/ohyung/domain/usecase/GetCurrentLocationForecastUseCase.kt
@@ -0,0 +1,39 @@
+/*
+ * Created by Lee Oh Hyoung on 2020/07/11 .. 
+ */
+package kr.ohyung.domain.usecase
+
+import io.reactivex.Scheduler
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.BiFunction
+import io.reactivex.schedulers.Schedulers
+import kr.ohyung.domain.entity.Forecast
+import kr.ohyung.domain.entity.LocationLegalName
+import kr.ohyung.domain.repository.ReverseGeocodingRepository
+import kr.ohyung.domain.repository.WeatherRepository
+
+class GetCurrentLocationForecastUseCase(
+    private val lat: Double,
+    private val lon: Double,
+    private val weatherRepository: WeatherRepository,
+    private val reverseGeocodingRepository: ReverseGeocodingRepository,
+    executorScheduler: Scheduler = Schedulers.io(),
+    postExecutionScheduler: Scheduler = AndroidSchedulers.mainThread()
+) : SingleUseCase<Forecast>(executorScheduler, postExecutionScheduler) {
+
+    override fun buildUseCaseSingle(): Single<Forecast> =
+        Single.zip(
+            weatherRepository.getWeatherByLatLon(lat, lon),
+            reverseGeocodingRepository.getCurrentLegalName(lat, lon),
+            BiFunction { emptyCityForecast: Forecast, legalName: LocationLegalName ->
+                Forecast(
+                    country = legalName.countryCode,
+                    city = legalName.city,
+                    current = emptyCityForecast.current,
+                    hourly = emptyCityForecast.hourly,
+                    daily = emptyCityForecast.daily
+                )
+            }
+        )
+}
